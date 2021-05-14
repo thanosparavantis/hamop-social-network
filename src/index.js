2427755587abf41ccabcd8ff9bcbe286fb6a603d
Thanos Paravantis <thanosparavantis@gmail.com>
2021-05-14 21:38:08 +0300

    Updates

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './tailwind.build.css'
-import HomePage from './home/HomePage';
-
-import JavascriptTimeAgo from 'javascript-time-ago'
-import el from 'javascript-time-ago/locale/el'
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import NavbarDesktop from "./NavbarDesktop";
-import AnnouncementsPage from "./announcements/AnnouncementsPage";
-import Helmet from "react-helmet";
-import ScrollToTop from "./ScrollToTop";
-
-JavascriptTimeAgo.addLocale(el);
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet>
-      <body className="bg-gray-200 font-hamop"/>
-    </Helmet>
-    <BrowserRouter>
-      <ScrollToTop/>
-      <NavbarDesktop/>
-      <Switch>
-        <Route path="/announcements">
-          <AnnouncementsPage/>
-        </Route>
-        <Route path="/">
-          <HomePage/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
