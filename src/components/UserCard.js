a1e106e46aa3ba325fa97ef5cb1100e616af9b7e
Thanos Paravantis <thanosparavantis@gmail.com>
2021-05-21 13:41:08 +0300

    Updates

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,7 +5,7 @@ import {faCircleNotch} from "@fortawesome/free-solid-svg-icons";
 import useUser from "../hooks/useUser";
 import StarsBadge from "./StarsBadge";
 
-function UserCard({userId, size = "normal", className = ""}) {
+function UserCard({userId, className = ""}) {
   const [user, loading, error] = useUser(userId)
 
   if (error) {
@@ -30,44 +30,23 @@ function UserCard({userId, size = "normal", className = ""}) {
       </div>
     )
   } else {
-    if (size === "small") {
-      return (
-        <Link to={`/${user.username}`}
-              className={`flex items-center bg-white p-3 rounded shadow focus:ring ${className}`}>
-          <div>
-            <img src={user.photoURL} alt={user.username} className="h-12 rounded shadow-lg border"/>
+    return (
+      <Link to={`/${user.username}`}
+            className={`flex items-center bg-white p-5 rounded shadow focus:ring ${className}`}>
+        <img src={user.photoURL} alt={user.username} className="h-12 md:h-20 rounded shadow-lg border"/>
+        <div className="ml-5">
+          <div className="text-gray-900 font-bold leading-none">
+            {user.displayName}
           </div>
-          <div className="ml-5 text-gray-600">
-            <div className="font-bold">
-              {user.displayName}
-            </div>
-            <div className="text-xs">
-              @{user.username}
-            </div>
-          </div>
-        </Link>
-      )
-    } else {
-      return (
-        <Link to={`/${user.username}`}
-              className={`flex items-center bg-white p-5 rounded shadow focus:ring ${className}`}>
-          <div>
-            <img src={user.photoURL} alt={user.username} className="h-20 rounded shadow-lg border"/>
-          </div>
-          <div className="ml-5">
-            <div className="text-gray-900 font-bold leading-none">
-              {user.displayName}
-            </div>
-            <div className="flex items-start flex-col md:flex-row md:items-center mt-1">
-              <StarsBadge user={user} className="mb-1 md:mr-2 md:mb-0"/>
-              <div className="text-sm text-gray-600">
-                Γράφτηκε <TimeAgo datetime={user.creationDate} locale="el"/>
-              </div>
+          <div className="flex items-center mt-2">
+            <StarsBadge user={user} className="mr-2"/>
+            <div className="text-xs md:text-sm text-gray-600">
+              Γράφτηκε <TimeAgo datetime={user.creationDate} locale="el"/>
             </div>
           </div>
-        </Link>
-      )
-    }
+        </div>
+      </Link>
+    )
   }
 }
 
