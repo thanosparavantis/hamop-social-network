2fc5d7c2ae258ace24ee2655c685cbbd065dc662
Thanos Paravantis <thanosparavantis@gmail.com>
2021-05-24 16:33:36 +0300

    Updates

--- src/Topics.js
+++ src/Topics.js
@@ -3,6 +3,8 @@ const topics = [
   {id: "news", title: "Ειδήσεις", path: "/topic/news"},
   {id: "events", title: "Events", path: "/topic/events"},
   {id: "food", title: "Φαγητά", path: "/topic/food"},
+  {id: "health", title: "Υγεία", path: "/topic/health"},
+  {id: "technology", title: "Τεχνολογία", path: "/topic/technology"},
   {id: "games", title: "Παιχνίδια", path: "/topic/games"},
   {id: "books", title: "Βιβλία", path: "/topic/books"},
   {id: "movies", title: "Ταινίες", path: "/topic/movies"},
